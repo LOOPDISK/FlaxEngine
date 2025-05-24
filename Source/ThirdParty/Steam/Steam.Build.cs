ccfcab9db11245c3ad8b32bf51a9bd53f11c10fd
Cissy Yu <cissy.yu.808@gmail.com>
2025-05-24 15:30:35 +0800

    steamworks api added and dependecy files added to output folder

---
+++ Source/ThirdParty/Steam/Steam.Build.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Wojciech Figat. All rights reserved.
+
+using System.IO;
+using Flax.Build;
+using Flax.Build.NativeCpp;
+
+/// <summary>
+/// Steamworks C++ API 
+/// </summary>
+public class Steam : DepsModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.Custom;
+        LicenseFilePath = "License.txt";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+
+    /// <inheritdoc />
+    public override void Setup(BuildOptions options)
+    {
+        base.Setup(options);
+
+        var depsRoot = options.DepsFolder;
+        options.DependencyFiles.Add(Path.Combine(depsRoot, "steam_api64.dll"));
+        options.DependencyFiles.Add(Path.Combine(depsRoot, "steam_appid.txt"));
+    }
+}
